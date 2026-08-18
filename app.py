802eb43c24bae6eaba965bece57433e1e10930c6
Manognya-1907 <manognyatipireddy@gmail.com>
2026-08-18 11:09:04 +0530

    Fix FastAPI app title formatting

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ formatted_agent_chain = (
 
 # --- 3. FastAPI App ---
 ##Need To Code
-app=FASTAPI("Indian Weather and Cinema Agent")
+app=FASTAPI(title="Indian Weather and Cinema Agent")
 add_routes(app,formatted_agent_chain,path="/agent",playground_type="default")
 
 if __name__ == "__main__":
